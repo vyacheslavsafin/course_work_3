968a3813324fce56d1b97c9f730928ae8eabc31b
vyacheslavsafin <safin.vr@mail.ru>
2023-06-21 01:12:57 +0300

    Добавлена функция get_filtered_data в utils.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from utils import get_data, get_filtered_data
+
+
+def main():
+
+    data = get_data()
+    data = get_filtered_data(data)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def get_data():
         data = json.load(file)
     return data
 
+
+def get_filtered_data(data):
+    '''Фильтрует данные по наличию ключа "state" в словаре и его значению "EXECUTED"'''
+
+    #print(f"До фильтрации: { len(data) }")
+    #print(f"Без ключа \"state\": { [x for x in data if 'state' not in x] }")
+
+    data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
+
+    #print(f"После фильтрации: {len(data)}")
+
+    return data
