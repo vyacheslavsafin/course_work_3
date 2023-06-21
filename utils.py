b368178caa05170917040812236abec0a7b31464
vyacheslavsafin <safin.vr@mail.ru>
2023-06-21 19:11:23 +0300

    PEP8 поправки

--- utils.py
+++ utils.py
@@ -1,21 +1,22 @@
 import json
 from datetime import datetime
 
+
 def get_data():
-    '''Загружает данные из файла и возвращает список словарей для дальнейшей обработки'''
+    """Загружает данные из файла и возвращает список словарей для дальнейшей обработки"""
     with open('operations.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     return data
 
 
 def get_filtered_data(data):
-    '''Фильтрует данные по наличию ключа "state" в словаре и его значению "EXECUTED"'''
+    """Фильтрует данные по наличию ключа "state" в словаре и его значению 'EXECUTED'"""
     data = [x for x in data if "state" in x and x["state"] == "EXECUTED"]
     return data
 
 
 def get_last_values(data, count_last_values):
-    '''Сортирует данные по дате и выводит последние пять транзакций'''
+    """Сортирует данные по дате и выводит последние пять транзакций"""
 
     def key_sort(x):
         return x["date"]
